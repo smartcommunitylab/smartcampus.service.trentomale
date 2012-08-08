9ff4a723dfe4910489e18b2b37fc6c678114dcb1
adami <adami@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-08-08 08:54:12 +0000

    Method to get a list of trains not tied to stations.

---
+++ src/main/java/smartcampus/service/trentomale/helper/TrainsBuilder.java
@@ -0,0 +1,73 @@
+package smartcampus.service.trentomale.helper;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.BindingProvider;
+
+import org.apache.commons.collections.MultiHashMap;
+import org.apache.commons.collections.MultiMap;
+
+import smartcampus.service.trentomale.wsclient.ArrayOfStation;
+import smartcampus.service.trentomale.wsclient.ArrayOfTrain;
+import smartcampus.service.trentomale.wsclient.Station;
+import smartcampus.service.trentomale.wsclient.StationStop;
+import smartcampus.service.trentomale.wsclient.Train;
+import smartcampus.service.trentomale.wsclient.TrainListTransport;
+import smartcampus.service.trentomale.wsclient.TrainViewService;
+import smartcampus.service.trentomale.wsclient.TrainViewService_Service;
+
+import com.google.protobuf.Message;
+
+public class TrainsBuilder {
+
+	public static List<Message> buildTrains() throws DatatypeConfigurationException {
+		TrainViewService_Service service = new TrainViewService_Service();
+		TrainViewService port = service.getBasicHttpBindingTrainViewService();
+		BindingProvider bp = (BindingProvider) port;
+		bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://trainview.algorab.net/TrainViewService.svc");
+		GregorianCalendar gcal = new GregorianCalendar();
+		ArrayOfStation stationsArray = port.getStations();
+
+
+		List<Integer> inputStationsId = new ArrayList<Integer>();
+		Map<Integer, String> stationsId = new TreeMap<Integer, String>();
+		MultiMap stationsTimes = new MultiHashMap();
+
+		XMLGregorianCalendar xgcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+		TrainListTransport list = port.getTrains(xgcal);
+		ArrayOfTrain trainsArray = list.getTrains().getValue();
+		List<Message> result = new ArrayList<Message>();
+
+		MultiMap tmpTrains = new MultiHashMap();
+
+		for (Train train : trainsArray.getTrain()) {
+			int lastStopN = train.getNextStops().getValue().getStationStop().size() - 1;
+			StationStop lastStop = train.getNextStops().getValue().getStationStop().get(lastStopN);
+			smartcampus.service.trentomale.data.message.Trentomale.Train.Builder trainBuilder = smartcampus.service.trentomale.data.message.Trentomale.Train.newBuilder();
+			trainBuilder.setId(train.getTrainId());
+			trainBuilder.setNumber(train.getTrainNumber());
+			trainBuilder.setDelay(train.getCurrentDelay());
+//			String minute = "" + stop.getArrivalMinute();
+//			minute = (minute.length() < 2) ? ("0" + minute) : minute;
+//			trainBuilder.setTime(stop.getArrivalHour() + ":" + minute);
+//			trainBuilder.setDirection(stationsId.get(lastStop.getStationId()));
+
+			smartcampus.service.trentomale.data.message.Trentomale.Train trainProto = trainBuilder.build();
+
+			result.add(trainProto);
+		}
+
+		return result;
+	}
+
+}
--- src/test/java/smartcampus/service/trentomale/test/Test.java
+++ src/test/java/smartcampus/service/trentomale/test/Test.java
@@ -3,13 +3,16 @@ package smartcampus.service.trentomale.test;
 import javax.xml.datatype.DatatypeConfigurationException;
 
 import smartcampus.service.trentomale.helper.StationBuilder;
+import smartcampus.service.trentomale.helper.TrainsBuilder;
 
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
 
 	public void test() throws DatatypeConfigurationException {
-		System.out.println(StationBuilder.buildStations("Trento,Trento Nord,Gardolo"));
+//		System.out.println(StationBuilder.buildStations("Trento,Cles"));
+		
+		System.out.println(TrainsBuilder.buildTrains());
 	}
 	
 }
