5f08b04589f4ee47a7867d0536c54089cba9b8ec
raman <raman@fbk.eu>
2014-12-02 09:47:01 +0100

    api update

--- src/test/java/smartcampus/service/trentomale/test/TestDataFlow.java
+++ src/test/java/smartcampus/service/trentomale/test/TestDataFlow.java
@@ -15,15 +15,24 @@
  ******************************************************************************/
 package smartcampus.service.trentomale.test;
 
+import it.sayservice.platform.client.InvocationException;
+import it.sayservice.platform.client.ServiceBusClient;
+import it.sayservice.platform.client.jms.JMSServiceBusClient;
 import it.sayservice.platform.core.bus.common.exception.PersistenceException;
 import it.sayservice.platform.core.common.exception.EntityNotFoundException;
 import it.sayservice.platform.core.common.exception.ServiceException;
+import it.sayservice.platform.core.message.Core.ActionInvokeParameters;
 import it.sayservice.platform.servicebus.test.DataFlowTestHelper;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.jms.ConnectionFactory;
+
 import junit.framework.TestCase;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 import smartcampus.service.trentomale.impl.TrainsTrentoMaleDataFlow;
 
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -31,13 +40,24 @@ import com.google.protobuf.InvalidProtocolBufferException;
 public class TestDataFlow extends TestCase {
 	public void testRun() throws PersistenceException, EntityNotFoundException, InvalidProtocolBufferException, ServiceException {
 		
-		DataFlowTestHelper helper = new DataFlowTestHelper();
+		DataFlowTestHelper helper = new DataFlowTestHelper("test");
 
 		Map<String, Object> parameters = new HashMap<String, Object>();
 
 		Map<String, Object> out = helper.executeDataFlow(
 				"smartcampus.service.TrentoMale", "TrainsTrentoMale", new TrainsTrentoMaleDataFlow(), parameters);
 		System.err.println(out);
+	}
+
+	public void testRemote() throws InvocationException {
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		ServiceBusClient client = new JMSServiceBusClient(connectionFactory);
+		client.setClientId("test_subscriber");
+		
+		ActionInvokeParameters invokeService = client.invokeService("smartcampus.service.TrentoMale", "TrainsTrentoMale", parameters);
+		System.err.println(invokeService.getDataCount());
 	}
 
 }
